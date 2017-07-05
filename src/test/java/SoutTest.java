2afe9a3665edae37e7137e06a6186e3ff2665fac
xjh <xiong_jinhua@foxmail.com>
2017-07-05 09:43:45 +0800

    add junit file

---
+++ src/test/java/SoutTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Jinhua on 2017/7/5 9:42.
+ */
+public class SoutTest {
+    
+    @Test
+    public void test(){
+        System.out.println("F*** the gradle");
+    }
+}
